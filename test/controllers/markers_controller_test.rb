98cc5b859022e77ae979aded830d5317c6991e82
Wietse <wietse.l.devries@gmail.com>
2015-10-01 11:42:02 +0200

    removing test folder

--- test/controllers/markers_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MarkersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/helpers/markers_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class MarkersHelperTest < ActionView::TestCase
-end
