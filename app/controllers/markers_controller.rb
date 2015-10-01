d617c85c06d0a89f3bccc54a23dd47d9afac6e7a
Steven Kaan <stevenkaan@gmail.com>
2015-10-01 14:56:12 +0200

    added deleting markers and editing

--- app/controllers/markers_controller.rb
+++ app/controllers/markers_controller.rb
@@ -10,10 +10,33 @@ class MarkersController < ApplicationController
 	    @city = City.find(params[:city_id])
 	    @marker = @city.markers.create(marker_params)
 	    redirect_to city_path(@city)
-  	end
- 
-	private
-	def marker_params
-		params.require(:marker).permit(:name)
 	end
+
+	def edit
+		@marker = Marker.find(params[:id])
+
+	end
+
+	def update
+		@marker = Marker.find(params[:id])
+
+		if @marker.update(marker_params)
+			@city = City.find(@marker.city_id)
+			redirect_to @city
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+			@marker = Marker.find(params[:id])
+			@city = City.find(@marker.city_id)
+			@marker.destroy
+			redirect_to @city
+	end
+
+	private
+		def marker_params
+			params.require(:marker).permit(:name)
+		end
 end
