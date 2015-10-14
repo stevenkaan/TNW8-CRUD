7c2933ff4ee40e32b07c2290cabe93be316da5e4
Friso <friso.obbema@gmail.com>
2015-10-14 10:08:09 +0200

    audio

--- app/uploaders/marker_audio_uploader.rb
+++ app/uploaders/marker_audio_uploader.rb
@@ -13,7 +13,6 @@ class MarkerAudioUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    'public/uploads/marker_audio'
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
