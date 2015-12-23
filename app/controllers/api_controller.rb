f74f1994cf23c62c4b18f765f1c1d5378c06be26
Wietse de Vries <wietse.l.devries@gmail.com>
2015-12-23 19:27:57 +0100

    fixing seed file

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -27,7 +27,7 @@ class ApiController < ApplicationController
 		language = params[:lang];
 
 		@city = City.where(id: params[:id]);
-		
+
 		if @city.length == 0
 			render :json => '404'
 			return
@@ -39,16 +39,16 @@ class ApiController < ApplicationController
 		country = Country.where(id: @city[0].country_id)
 		city_info = CityInfo.where(city_id: @city[0].id, language: language);
 
-		if language == 'eng' or language == 'nld' or language == 'esp' 	
+		if language == 'eng' or language == 'nld' or language == 'esp'
 
 			for marker in @markers
 				marker_info = MarkerInfo.where(marker_id: marker.id, language: language);
-				if(marker.marker_lat != nil && marker.marker_lng != nil) 
+				if(marker.marker_lat != nil && marker.marker_lng != nil)
 					markers_array.push( id: marker.id, name: marker_info[0].name, info: marker_info[0].body, type: marker.marker_type, icon_only: marker.icon_only, latitude: marker.marker_lat, longitude: marker.marker_lng, audio: marker_info[0].marker_audio.url, image_1: marker.marker_images.url, image_2: marker.marker_images_2.url, image_3: marker.marker_images_3.url, image_4: marker.marker_images_4.url );
 				end
 			end
-			
-			if language == 'nld' 
+
+			if language == 'nld'
 				for route in @routes
 					routes_array.push( id: route.id, name: route.name_nld, info: route.info_nld, distance: route.afstand, multiple_startingpoints: route.multiple_starting_points, markers: route.return_markers);
 				end
@@ -57,7 +57,7 @@ class ApiController < ApplicationController
 
 			end
 
-			if language == 'eng' 
+			if language == 'eng'
 				for route in @routes
 					routes_array.push( id: route.id, name: route.name_eng, info: route.info_eng, distance: route.afstand, multiple_startingpoints: route.multiple_starting_points, markers: route.return_markers);
 				end
@@ -74,13 +74,13 @@ class ApiController < ApplicationController
 				result = { id: @city[0].id, name: city_info[0].name, info: city_info[0].body, country: country[0].country_name_esp, image_1: @city[0].city_images.url, image_2: @city[0].city_images_2.url, image_3: @city[0].city_images_3.url, image_4: @city[0].city_images_4.url, markers: markers_array, routes: routes_array };
 			end
 
-			
+
 		end
 
 		if @city[0].get_languages.include?(language)
 			render :json => {city: result}
 		else
-		
+
 			render :json => '404'
 		end
 
