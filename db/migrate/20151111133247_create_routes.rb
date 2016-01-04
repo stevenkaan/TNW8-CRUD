3ef1a6ebf2771e69fc060ae248411b3138c701cf
Friso <friso.obbema@gmail.com>
2016-01-04 12:22:36 +0100

    migratefix

--- db/migrate/20151111133247_create_routes.rb
+++ db/migrate/20151111133247_create_routes.rb
@@ -3,6 +3,7 @@ class CreateRoutes < ActiveRecord::Migration
     create_table :routes do |t|
       t.decimal :afstand
       t.references :city, index: true, foreign_key: true
+      
       t.timestamps
     end
   end
